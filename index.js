493db026a85e74a0adc3a0fe46be20b08eac1472
Eddie <eduard.raducanu09@gmail.com>
2023-01-03 21:27:07 +0200

    improved functionality
    Now made as a sandbox-type of "rock, paper, scissors", where the round reset is the refresh button.
    No more announcements about who wins, everything will be displayed on-screen (as well as the rounds won counter for player and for computer).
    Added a CSS file to beautify the whole content.

--- index.js
+++ index.js
@@ -2,63 +2,86 @@ let gameArr = ["rock", "paper", "scissors"];
 let playerWon = 0;
 let computerWon = 0;
 
-function computerPlay() {
+function computerPlay(){
     let computerAnswer = [""];
     let randomComputerNumber = Math.floor(Math.random()*3);
     return computerAnswer[""] = gameArr[randomComputerNumber];
 }
 
-function playRound(playerAnswer, computerAnswer) {
-    if (playerAnswer === "rock" && computerAnswer === "paper"){ 
-        console.log("Paper beats rock! Computer wins!");
-        computerWon = computerWon + 1; 
-    } else if (playerAnswer === "rock" && computerAnswer === "scissors"){
-        console.log("Rock beats scissors! Player wins!");
-        playerWon = playerWon + 1;
-    } else if (playerAnswer === "paper" && computerAnswer === "rock"){
-        console.log("Paper beats rock! Player wins!");
-        playerWon = playerWon + 1;
-    } else if (playerAnswer === "paper" && computerAnswer === "scissors"){
-        console.log("Scissors beats paper! Computer wins!");
-        computerWon = computerWon + 1; 
-    } else if (playerAnswer === "scissors" && computerAnswer === "paper"){ 
-        console.log("Scissors beats paper! Player wins!");
-        playerWon = playerWon + 1;
-    } else if (playerAnswer === "scissors" && computerAnswer === "rock"){
-        console.log("Rock beats scissors! Computer wins!");
-        computerWon = computerWon + 1; 
-    } else if (playerAnswer === computerAnswer){
-        console.log("Tie!");
-    }
-}
-
-function game(){
-    for (let i = 1; i <= 5; i++){
+document.getElementById('rock').addEventListener('click', function(){
+    let playerAnswer = 'rock';
+    function game(){
         computerAnswer = computerPlay();
-        let answerPrompt = prompt("Rock, paper or scissors?");
-        let playerAnswer = answerPrompt.toLowerCase().trim();
-        if (playerAnswer === "rock" || playerAnswer === "paper" || playerAnswer === "scissors"){
             playRound(playerAnswer, computerAnswer);    
-            if (playerWon === 3){
-                console.log("You won 3 out of 5 games! Game over.");
-                break;
-            } else if (computerWon === 3){
-                console.log("Computer won 3 out of 5 games! Game over.");
-                break;
-            }
-        }
-        else {
-            alert("Invalid answer. Try again.");
-            i--;
-        }
-    }
-    if (playerWon > computerWon){
-        alert("You won!");
-    } else if (playerWon < computerWon){
-        alert("Computer won!");
-    } else if (playerWon === computerWon){
-        alert("It's a tie!");
-    }
-}
+            // if (playerWon === 3){
+            //     alert("You won 3 out of 5 games! Game over.");
+            // } else if (computerWon === 3){
+            //     alert("Computer won 3 out of 5 games! Game over.");
+            // }
+    };
+    game();
+});
+document.getElementById('paper').addEventListener('click', function(){
+    let playerAnswer = 'paper';
+    function game(){
+        computerAnswer = computerPlay();
+            playRound(playerAnswer, computerAnswer);    
+            // if (playerWon === 3){
+            //     alert("You won 3 out of 5 games! Game over.");
+            // } else if (computerWon === 3){
+            //     alert("Computer won 3 out of 5 games! Game over.");
+            // }
+    };
+    game(); 
+});
+document.getElementById('scissors').addEventListener('click', function(){
+    let playerAnswer = 'scissors';
+    function game(){
+        computerAnswer = computerPlay();
+            playRound(playerAnswer, computerAnswer);    
+                // if (playerWon === 3){
+                //     alert("You won 3 out of 5 games! Game over.");
+                // } else if (computerWon === 3){
+                //     alert("Computer won 3 out of 5 games! Game over.");
+                // }
+    };
+    game(); 
+});
 
-game();
+function playRound(playerAnswer, computerAnswer){
+    if (playerAnswer === "rock" && computerAnswer === "paper"){ 
+        document.querySelector('h1').innerHTML = "Paper beats rock! Computer wins!";
+        computerWon = computerWon + 1; 
+        document.querySelector('#computerWon').innerHTML = `Rounds won by computer: ${computerWon}`;
+    } else if (playerAnswer === "rock" && computerAnswer === "scissors"){
+        document.querySelector('h1').innerHTML = "Rock beats scissors! Player wins!";
+        playerWon = playerWon + 1;
+        document.querySelector('#playerWon').innerHTML = `Rounds won by player: ${playerWon}`;
+    } else if (playerAnswer === "paper" && computerAnswer === "rock"){
+        document.querySelector('h1').innerHTML = "Paper beats rock! Player wins!";
+        playerWon = playerWon + 1;
+        document.querySelector('#playerWon').innerHTML = `Rounds won by player: ${playerWon}`;
+    } else if (playerAnswer === "paper" && computerAnswer === "scissors"){
+        document.querySelector('h1').innerHTML = "Scissors beats paper! Computer wins!";
+        computerWon = computerWon + 1; 
+        document.querySelector('#computerWon').innerHTML = `Rounds won by computer: ${computerWon}`;
+    } else if (playerAnswer === "scissors" && computerAnswer === "paper"){ 
+        document.querySelector('h1').innerHTML = "Scissors beats paper! Player wins!";
+        playerWon = playerWon + 1;
+        document.querySelector('#playerWon').innerHTML = `Rounds won by player: ${playerWon}`;
+    } else if (playerAnswer === "scissors" && computerAnswer === "rock"){
+        document.querySelector('h1').innerHTML = "Rock beats scissors! Computer wins!";
+        computerWon = computerWon + 1; 
+        document.querySelector('#computerWon').innerHTML = `Rounds won by computer: ${computerWon}`;
+    } else if (playerAnswer === computerAnswer){
+        document.querySelector('h1').innerHTML = "Tie!";
+    };
+};
+
+// if (playerWon > computerWon){
+            //     alert("You won!");
+            // } else if (playerWon < computerWon){
+            //     alert("Computer won!");
+            // } else if (playerWon === computerWon){
+            //     alert("It's a tie!");
+            // }
